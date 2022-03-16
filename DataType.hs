005191e51aab04bd21d0a1d08b59285bbd5e0d1f
Renan Zapelini <renanzapelini@hotmail.com>
2022-03-15 22:05:37 -0300

    Updates

---
+++ DataType.hs
@@ -0,0 +1,8 @@
+module DataType where
+
+data Mood = Woot | Blah deriving Show
+
+changeMood :: Mood -> Mood
+
+changeMood Blah = Woot
+changeMood Woot = Blah
---
+++ PatternMatching.hs
@@ -0,0 +1,8 @@
+module PatternMatching where
+
+getNumberExpandedForm :: Integer -> String
+
+getNumberExpandedForm 1 = "One"
+getNumberExpandedForm 2 = "Two"
+
+
